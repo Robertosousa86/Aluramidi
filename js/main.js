aeac2b3e18bab42fd0f4a7d912c87629dbe7bf90
Roberto Sousa <robertosousa186@gmail.com>
2022-03-01 01:15:30 -0300

    Updating while statement

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const listKey = document.querySelectorAll('.tecla');
 let counter = 0;
 
 while (counter < listKey.length) {
-  listKey[counter].onclick = playPom;
-
+  listKey[counter].onclick = () => playSong('#som_tecla_pom');
+  
   counter = counter + 1;
 }

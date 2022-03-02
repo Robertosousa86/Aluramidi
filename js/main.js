01078c410c578edd67873f10bc759d2a17a0f447
Roberto Sousa <robertosousa186@gmail.com>
2022-03-01 22:01:52 -0300

    Using for statement

--- js/main.js
+++ js/main.js
@@ -3,15 +3,11 @@ const playSong = (idAudioElement) =>
 
 const listKey = document.querySelectorAll('.tecla');
 
-let counter = 0;
-
-while (counter < listKey.length) {
+for (counter = 0; counter < listKey.length; counter++) {
   const key = listKey[counter];
   const instrument = key.classList[1];
 
   const idAudio = `#som_${instrument}`;
 
   key.onclick = () => playSong(idAudio);
-
-  ++counter;
 }
